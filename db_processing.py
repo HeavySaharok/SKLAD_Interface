41cba3fa0c29e6de4ab0d6e3cfc05ee1fbd53363
Gearworker <lab437861@mail.ru>
2024-04-22 10:24:59 +0300

    BD-update

--- db_processing.py
+++ db_processing.py
@@ -3,7 +3,7 @@ import sqlite3
 
 con = sqlite3.connect("main_storage.db")
 cur = con.cursor()
-result = cur.execute("""SELECT * FROM basic_store
+result = cur.execute("""SELECT * FROM products
             WHERE type = 1""").fetchall()
 for elem in result:
     print(elem)
