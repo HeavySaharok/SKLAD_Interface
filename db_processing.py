b20a110cdb4b7c44ec73d9a99eba1276f5a00e98
Gearworker <lab437861@mail.ru>
2024-04-24 16:53:54 +0300

    Data update

---
+++ db_processing.py
@@ -0,0 +1,27 @@
+# Импорт библиотеки
+import sqlite3
+
+
+def create_table(name):
+    # Подключение к БД
+    con = sqlite3.connect("db/main_storage.db")
+
+    # Создание курсора
+    cur = con.cursor()
+
+    # Выполнение запроса и получение всех результатов
+    result = cur.execute(f"""CREATE TABLE "{name}" (
+        "id"	TEXT NOT NULL,
+        "name"	TEXT NOT NULL,
+        "category"	INTEGER NOT NULL,
+        "amount"	INTEGER NOT NULL,
+        "price"	INTEGER NOT NULL,
+        "weight"	INTEGER NOT NULL,
+        PRIMARY KEY("id")
+    )""")
+
+    con.commit()
+    con.close()
+
+
+create_table('WARE_B')
